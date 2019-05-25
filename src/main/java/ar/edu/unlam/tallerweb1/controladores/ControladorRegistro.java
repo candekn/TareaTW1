d8615eb891e24c271bf26972dc444d876887e92b
DianaAguayo <ddianaosi15@gmail.com>
2019-05-25 12:30:06 -0300

    Agrego action para realizar consulta

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorRegistro.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorRegistro.java
@@ -1,5 +1,8 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,12 +14,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unlam.tallerweb1.modelo.Usuario;
+import ar.edu.unlam.tallerweb1.servicios.ServicioBuscar;
 import ar.edu.unlam.tallerweb1.servicios.ServicioRegistro;
 
 @Controller
 public class ControladorRegistro {
 	@Inject
 	private ServicioRegistro servicioRegistro;
+	
+	
+	@Inject
+	private ServicioBuscar serviciobuscar;
+
+	
 	
 	@RequestMapping("/registro")
 	public ModelAndView registro(){
@@ -26,11 +36,39 @@ public class ControladorRegistro {
 		return new ModelAndView("RegistroTarea", model);
 	}
 	
+	
+	
 	@RequestMapping(path="/validarRegistro", method=RequestMethod.POST)
 	public ModelAndView ValidarRegistro(@ModelAttribute("usuario") Usuario usuario, HttpServletRequest request){
 		servicioRegistro.guardarUsuario(usuario);
+	    
 		return new ModelAndView("RegistroTarea");
 	}
+	
+	
+	
+	@RequestMapping(path = "/MuestroDatosBD")	
+	public ModelAndView consultarDatosenBD()
+		{
+		ModelMap modelo = new ModelMap();
+		
+		//Atributo tipo Interfaz . Metodo en Interfaz
+		
+		List <Usuario> usuarioBuscado = serviciobuscar.buscarDatos();
+		if (usuarioBuscado.isEmpty() ) {
+			modelo.put("mensaje", "Usuario no encontrado");
+			
+			
+		} else {
+			// si el usuario no existe agrega un mensaje de error en el modelo.
+			modelo.put("mensaje", "Usuario encontrado");
+		}
+		modelo.put("usuarioBuscado", usuarioBuscado);
+		
+		return new ModelAndView("DatosBD",modelo);
+		}
+
+
 	
 	
 	
